b9ba17581129fb034622a491252aeac86a8d82dd
m_hammad-ur-rehman <164639583+hammadurrehman2006@users.noreply.github.com>
2025-04-08 10:51:47 +0500

    Update 15_final_project.ino

--- 15_final_project/15_final_project.ino
+++ 15_final_project/15_final_project.ino
@@ -4,66 +4,93 @@
 #define WARNING_LED_PIN 11
 
 // ultrasonic
-unsigned long lastUltrasonicTrigger = millis();
+unsigned long lastTimeUltrasonicTrigger = millis();
 unsigned long ultrasonicTriggerDelay = 60;
 
-unsigned long pulseInTimeBegin;
-unsigned long pulseInTimeEnd;
-bool newDistAvailable = false;
+volatile unsigned long pulseInTimeBegin;
+volatile unsigned long pulseInTimeEnd;
+volatile bool newDistanceAvailable = false;
 
-double prevDistance = 400.0;
+double previousDistance = 400.0;
 
-// warning led 
-unsigned long int lastTimeWLedBlink = millis();
-unsigned long warningLedDelay = 500;
-byte warningLedState = LOW;
+// warning LED
+unsigned long lastTimeWarningLEDBlinked = millis();
+unsigned long warningLEDDelay = 500;
+byte warningLEDState = LOW;
 
-void triggerSensor() {
+void triggerUltrasonicSensor()
+{
   digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIGGER_PIN, LOW);
 }
-void echoPinInterrupt(){
-  if(digitalRead(ECHO_PIN) == HIGH){
-    pulseInTimeBegin = micros();
-  }
-  else{
-    pulseInTimeEnd = micros();
-    newDistAvailable = true;
-  }
-}
 
-double getUltrasonicDist(){
-  double distance = (pulseInTimeEnd - pulseInTimeBegin) / 58.0;
-  if(distance > 400.0){
-    return prevDistance;
+double getUltrasonicDistance()
+{
+  double durationMicros = pulseInTimeEnd - pulseInTimeBegin;
+  double distance = durationMicros / 58.0;
+  if (distance > 400.0) {
+    return previousDistance;
   }
-  distance = prevDistance * 0.6 + distance * 0.4;
-  prevDistance = distance;
+  distance = previousDistance * 0.6 + distance * 0.4;
+  previousDistance = distance;
   return distance;
 }
 
+void echoPinInterrupt()
+{
+  if (digitalRead(ECHO_PIN) == HIGH) { // pulse started
+    pulseInTimeBegin = micros();
+  }
+  else { // pulse stoped
+    pulseInTimeEnd = micros();
+    newDistanceAvailable = true;
+  }
+}
+
+void toggleWarningLED()
+{
+  warningLEDState = (warningLEDState == HIGH) ? LOW : HIGH;
+  digitalWrite(WARNING_LED_PIN, warningLEDState);
+}
+
+void setWarningLEDBlinkRateFromDistance(double distance)
+{
+  // 0 .. 400 cm -> 0 .. 1600 ms
+  warningLEDDelay = distance * 4;
+  Serial.println(warningLEDDelay);
+}
 
 void setup() {
   Serial.begin(115200);
-  pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
+  pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(WARNING_LED_PIN, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(ECHO_PIN), echoPinInterrupt, CHANGE);
+
+  attachInterrupt(digitalPinToInterrupt(ECHO_PIN),
+                  echoPinInterrupt,
+                  CHANGE);
 }
 
 void loop() {
   unsigned long timeNow = millis();
-  if (timeNow - lastUltrasonicTrigger > ultrasonicTriggerDelay) {
-    lastUltrasonicTrigger += ultrasonicTriggerDelay;
+
+  if (timeNow - lastTimeUltrasonicTrigger > ultrasonicTriggerDelay) {
+    lastTimeUltrasonicTrigger += ultrasonicTriggerDelay;
+    triggerUltrasonicSensor();
   }
-  if (newDistAvailable) {
-    newDistAvailable = false;
-    double distance = getUltrasonicDist();
-    Serial.print("Distance: ");
-    Serial.println(distance);
+
+  if (timeNow - lastTimeWarningLEDBlinked > warningLEDDelay) {
+    lastTimeWarningLEDBlinked += warningLEDDelay;
+    toggleWarningLED();
   }
-    
+
+  if (newDistanceAvailable) {
+    newDistanceAvailable = false;
+    double distance = getUltrasonicDistance();
+    setWarningLEDBlinkRateFromDistance(distance);
+    // Serial.println(distance);
+  }
 }
